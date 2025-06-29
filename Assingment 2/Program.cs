47dfb6375be669357565fadd6cb84ff88fabaec2
Ibrahim-Mohamedd1 <ibrahimzaazaa56@gmail.com>
2025-06-29 21:48:14 +0300

    Assingment 2

--- Assingment 2/Program.cs
+++ Assingment 2/Program.cs
@@ -4,7 +4,69 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region probelm 1
+            //Console.Write("Enter a Nymber : ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"Your Number is : {number}");
+            #endregion
+            #region problem 2
+            //string name = "Bemo";
+            //Convert.ToInt32(name);
+            //Unhandled exception. System.FormatException: The input string 'Bemo' was not in a correct format.
+            #endregion
+            #region problem 3
+            //float x = 2.6f;
+            //float y = 2.4f;
+            //float result = x + y;
+            //Console.WriteLine(result);
+            #endregion
+            #region problem 4
+            //string name = "I'm Bemo";
+            //string name1 = name.Substring(4, 4);
+            //Console.WriteLine(name);
+            //Console.WriteLine(name1);
+            #endregion
+            #region problem 5
+            //int x = 10;
+            //int y = x;
+            //Console.WriteLine($"Before modify x = {x}, y = {y}");
+            //y = 5;
+            //Console.WriteLine($"After modify x = {x}, y = {y}");
+            #endregion
+            #region problem 6
+            //Class1 c1 = new Class1();
+            //c1.name = "Bemo";
+            //Class1 c2 = c1;
+            //Console.WriteLine($"Before modify c1 = {c1.name} , c2 = {c2.name}");
+            //c2.name = "Bemixz";
+            //Console.WriteLine($"After modify c1 = {c1.name} , c2 = {c2.name}");
+            #endregion
+            #region problem 7
+            //Console.Write("Enter First Name: ");
+            //string st1 = Console.ReadLine();
+            //Console.Write("Enter Secound Name: ");
+            //string st2 = Console.ReadLine();
+            //Console.WriteLine($"Your name is {st1} {st2}");
+            //Console.WriteLine(st1 + st2);
+            #endregion
+            #region problem 8
+            //int d;
+            //d = Convert.ToInt32(!(30 < 20));
+            ////A value 1 will be assigned to d.
+            #endregion
+            #region problem 9
+            //Console.WriteLine(13 / 2 + " " + 13 % 2);
+            ////6 1
+            #endregion
+            #region problemm 10
+            //int num = 1, z = 5;
+
+            //if (!(num <= 0))
+            //    Console.WriteLine(++num + z++ + " " + ++z);
+            //else
+            //    Console.WriteLine(--num + z-- + " " + --z);
+            ////7 7
+            #endregion
         }
     }
 }
